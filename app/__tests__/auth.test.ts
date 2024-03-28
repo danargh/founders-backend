d9c208c55773cc9e3e06597d720ddbe32a540759
danargh <danargh86@gmail.com>
2024-03-28 21:26:43 +0700

    feat: adding register unit test

--- app/__tests__/auth.test.ts
+++ app/__tests__/auth.test.ts
@@ -1,8 +1,10 @@
 import request from "supertest";
 import app from "../index";
 import { closeDBTesting, connectDBTesting } from "./test.utils";
+import { deleteUserByEmail } from "../models/users";
 
-describe("Register /auth/login", () => {
+// auth login
+describe("Login /auth/login", () => {
    beforeAll(async () => {
       await connectDBTesting();
    });
@@ -17,6 +19,33 @@ describe("Register /auth/login", () => {
          password: "12345678",
       });
       expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty("data");
+   });
+});
+
+// auth register
+describe("Register /auth/register", () => {
+   beforeAll(async () => {
+      await connectDBTesting();
+      await deleteUserByEmail("danar@gmail.com");
+   });
+   afterAll(async () => {
+      await closeDBTesting();
+   });
+
+   // success case
+   test("It should return 200 and a token", async () => {
+      const response = await request(app).post("/auth/register").send({
+         email: "danar@gmail.com",
+         username: "danar",
+         password: "12345678",
+         femaleName: "Tunarsih",
+         maleName: "Sobirin",
+         websiteUrl: "https://polokrami.com",
+         phone: "08123456789",
+         terms: true,
+      });
+      expect(response.status).toBe(201);
       expect(response.body).toHaveProperty("data");
    });
 });
